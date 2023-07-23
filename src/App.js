2b81d6311dba7125055d7e5940e8cab611564546
senichek <senichek@gmail.com>
2023-07-23 23:12:21 +0200

    aws demo initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const msg = "Awesome app!";
 
 function App() {
+
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [isLogged, setIsLogged] = useState(false);
+
+  const login = (usr, pass) => {
+    if (usr === "JohnDoe" && pass === "pass111") {
+      setIsLogged(true);
+    } else {
+      setIsLogged(false);
+    }
+  }
+
+  const loginForm = () => {
+    return (
+      <form>
+        <label for="uname"><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="uname" required onChange={(event) => setUsername(event.target.value)}></input>
+  
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required onChange={(event) => setPassword(event.target.value)}></input>
+  
+      <button type="submit" onClick={() => login(username, password)}>Login</button>
+      </form>
+    )
+  }
+
+  const welcomePage = () => {
+    return (<div>You have been logged, {username}!</div>)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {msg}
+      {isLogged}
+      {isLogged ? welcomePage() : loginForm()}
     </div>
   );
 }
